082b7100664cfc63eefb87b81e9d03122a9c881b
MD. Juwel Rana <rk370613@gmail.com>
2024-05-15 11:46:28 +0600

    j

--- index.js
+++ index.js
@@ -1,36 +1,70 @@
-let carosulslides = document.querySelectorAll(".absolute");
-let i = 0;
+let todoForm = document.getElementById("todoForm")
+let todoInput = document.getElementById("todoInput")
+let showTodo = document.getElementById("showTodo")
+let btn = document.getElementById("btn")
+let deletBtn = document.querySelector(".btn2")
 
-carosulslides.forEach((item, index) => {
-  item.style.left = `${index * 100}%`;
-});
 
-setInterval(() => {
-  carosulslides.forEach((slide) => {
-    slide.style.transform = `translateX(-${i * 100}%)`;
-  });
-  if (i < carosulslides.length) {
-    i++;
-  } else {
-    i = 0;
+const getTodoListFromLocal = () =>{
+  return JSON.parse(localStorage.getItem("todolist"))
+}
+
+let localTodoList = getTodoListFromLocal() || []
+
+
+const addtododinamic = (elem)=>{
+  createTodoElement(elem)
+}
+
+
+
+const addtodoList = (e)=>{
+ e.preventDefault()
+ let todovalue = todoInput.value.trim()
+
+todoInput.value = " "
+
+ localTodoList.push(todovalue)
+ localTodoList = [... new Set(localTodoList)]
+console.log(localTodoList);
+
+localStorage.setItem("todoList", JSON.stringify(localTodoList) )
+
+
+createTodoElement(todovalue)
+
+//  document.getElementById("todoInput").value = " "
+
+}
+
+ const showTodolist = ()=>{
+  localTodoList.forEach((elem)=>{
+addtododinamic(elem)
+console.log(elem);
+  })
+
+}
+
+showTodolist()
+
+btn.addEventListener("click",function(e){
+  addtodoList(e);
+
+
+})
+
+
+function createTodoElement(todovalue){
+  const div = document.createElement("div")
+  div.className = " showDiv"
+  div.innerHTML = `<li>⏩ ${todovalue}</li> <button class="btn2" >delet</button>`
+  showTodo.appendChild(div)
+
+  if(showTodo !== " " ){
+    showTodo.classList.add("showbg")
   }
-}, 3000);
-
-// script.js
-const carouselItems = document.querySelectorAll(".carousel-item");
-let currentIndex = 0;
-
-function showSlide(index) {
-  carouselItems.forEach((item, i) => {
-    item.classList.toggle("active", i === index);
-  });
 }
 
-function rotateSlides() {
-  currentIndex = (currentIndex + 1) % carouselItems.length;
 
-  showSlide(currentIndex);
-}
 
-// Set an interval for auto-rotation (adjust the timing as needed)
-setInterval(rotateSlides, 3500); // 3.5 seconds
+
